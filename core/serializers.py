e893c0d73415f00342eb29cbdd0451ab9fc5fa02
Maruf Khan <whoismaruf@outlook.com>
2021-09-05 15:20:56 +0600

    created serializers for Task

---
+++ core/serializers.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.db.models import fields
+from rest_framework import serializers
+from core.models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    tasker = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    class Meta:
+        model = Task
+        fields = ('pk', 'name', 'created_at', 'tasker')
