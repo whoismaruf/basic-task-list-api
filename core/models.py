e684cb18563898d6c65ad2fec235e8c3332c41b0
Maruf Khan <whoismaruf@outlook.com>
2021-09-05 15:20:35 +0600

    added models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    tasker = models.ForeignKey(User, on_delete=models.CASCADE)
