0c1fdcbaf5813ae0682a20dd75febb6f1fa1c212
Maruf Khan <whoismaruf@outlook.com>
2021-09-05 15:21:23 +0600

    added views

--- core/views.py
+++ core/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import HttpResponse
+from rest_framework import generics
+from core.models import Task
+from core.serializers import TaskSerializer
+from root.permissions import TaskerPermission
 
 # Create your views here.
 
 
-def TaskView(request):
-    return HttpResponse('It is working')
+class TaskView(generics.ListCreateAPIView):
+    def get_queryset(self):
+        return Task.objects.all().filter(
+            tasker=self.request.user
+        ).order_by('-created_at')
+    serializer_class = TaskSerializer
+    permission_classes = (TaskerPermission,)
+    def perform_create(self, serializer):
+        serializer.save(tasker=self.request.user)
+
+
+
+class TaskRetriveDestroyView(generics.RetrieveDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = (TaskerPermission,)
